ea20c13c1ccb4a27ecbb63111fb584643d677280
brockmeyer7 <brockmeyer05@gmail.com>
2023-02-21 12:41:23 -0600

    experiment with multithreading

--- comics_tracker/tracker/views.py
+++ comics_tracker/tracker/views.py
@@ -5,17 +5,23 @@ import calendar, time
 import requests, json
 import os
 from dotenv import load_dotenv
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 def index(request):
-    response = json.loads(str(character_lookup()))
     result = []
-    for item in response['data']['results']:
-        result.append(item['name'])
+    threads=[]
+    with ThreadPoolExecutor(max_workers=20) as executor:
+        for i in range(20):
+            threads.append(executor.submit(character_lookup, i * 100))
+            
+        for task in as_completed(threads):
+            for item in json.loads(task.result())['data']['results']:
+                result.append(item['name'])
     return HttpResponse(json.dumps(result))
 
-def character_lookup():
+def character_lookup(offset):
     auth_string = api_auth()
-    api_url = 'https://gateway.marvel.com:443/v1/public/characters?' + auth_string
+    api_url = 'https://gateway.marvel.com:443/v1/public/characters?limit=100&offset=' + str(offset) + '&' + auth_string
     response = requests.get(api_url)
     return json.dumps(response.json())
 
@@ -28,4 +34,3 @@ def api_auth():
     ts = str(calendar.timegm(gmt))
     hash = hashlib.md5((ts+private_key+public_key).encode())
     return ('ts=' + ts + '&apikey=' + public_key + '&hash=' + hash.hexdigest())
-
