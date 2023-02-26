6b078ba1a218be73220396c388aac181c871dce2
brockmeyer7 <brockmeyer05@gmail.com>
2023-02-25 22:06:00 -0600

    start work on book endpoint

--- comics_tracker/tracker/urls.py
+++ comics_tracker/tracker/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('book/<str:name>/<int:year>', views.book, name='book'),
+    path('book/<str:title>/<int:start_year>/<int:iss_start_num>/<int:iss_end_num>', views.book, name='book'),
     path('character/<str:name>', views.character, name = 'character')
 ]
--- comics_tracker/tracker/views.py
+++ comics_tracker/tracker/views.py
@@ -11,27 +11,45 @@ from django.views.decorators.http import require_http_methods
 def index(request):
     return HttpResponse("Hello World!")
 
-@require_http_methods(['GET'])
-def book(request):
+@require_http_methods(['GET', 'POST'])
+def book(request, title: str, start_year: int, iss_start_num: int, iss_end_num: int):
     if request.method == 'GET':
-        pass
+        result = []
+        api_response = json.loads(book_lookup(title, start_year, iss_start_num, iss_end_num))
+        for item in api_response:
+            item_dict = {}
+            item_dict['id'] = item['id']
+            result['issue_num'] = api_response['issueNumber']
+            item_dict['title'] = item['title']
+            item_dict['description'] = item['description']
+            result.append(item_dict)
+        return HttpResponse(json.dumps(result))
+
 
 @require_http_methods(['GET'])
 def character(request, name: str):
     if request.method == 'GET':
         result = {}
-        api_response = json.loads(character_lookup(name))
-        print(api_response['data']['results'][0]['name'])
-        # result['id'] = api_response['data']['results']['id']
-        # result['name'] = api_response['data']['results']['name']
-        # result['description'] = api_response['data']['results']['description']
-        return HttpResponse(character_lookup(name))
+        api_response = json.loads(character_lookup(name))['data']['results'][0]
+        result['id'] = api_response['id']
+        result['name'] = api_response['name']
+        result['description'] = api_response['description']
+        return HttpResponse(json.dumps(result))
 
 def character_lookup(name: str):
     auth_string = api_auth()
     api_url = 'https://gateway.marvel.com:443/v1/public/characters?name=' + name + '&' + auth_string
     response = requests.get(api_url)
     return json.dumps(response.json())
+
+def book_lookup(title: str, start_date: int, iss_start_num: int, iss_end_num: int):
+    auth_string = api_auth()
+    api_url = 'https://gateway.marvel.com:443/v1/public/comics?' + auth_string + '&title=' + title + '&startDate=' + str(start_date)
+    result = []
+    for i in range(iss_start_num, iss_end_num + 1):
+        response = requests.get(api_url + '&issueNumber=' + str(i))
+        result.append(response)
+    return json.dumps(result.json())       
 
 def api_auth():
     dotenv_path = os.path.expanduser('~/Documents/Personal_Projects/.env')
