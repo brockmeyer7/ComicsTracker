ef2a27ae0ee91fb86cb590345f84dc04b55b396f
brockmeyer7 <brockmeyer05@gmail.com>
2023-03-14 13:01:52 -0500

    add /api/ to end of self.api_url

--- comics_tracker/tracker/comicvine_wrapper.py
+++ comics_tracker/tracker/comicvine_wrapper.py
@@ -9,7 +9,7 @@ class ComicVine():
         dotenv.load_dotenv(os.path.expanduser('~/Documents/Personal_Projects/.env'))
         cv_api_key = os.getenv('COMICVINE_API_KEY')
         self.api_key = f'{cv_api_key}'
-        self.api_url = 'https://comicvine.gamespot.com/'
+        self.api_url = 'https://comicvine.gamespot.com/api/'
         self.headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
 
     def search(self, params={}):
@@ -18,9 +18,8 @@ class ComicVine():
             filter_string = k + '=' + v
             url += '&' + urllib.parse.quote(filter_string, safe='=')
         url += '&format=json'
-        print(url)
-        # response = requests.get(url, headers=self.headers)
-        # print(response.headers['content-type'])
+        response = requests.get(url, headers=self.headers)
+        print(response.json())
 
 if __name__ == '__main__':
     cv = ComicVine()
